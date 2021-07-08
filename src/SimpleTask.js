014ee7d29f6aefa2da56cac67e01e491775296fe
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-07-08 17:04:09 -0600

    style: apply border radious

--- src/SimpleTask.js
+++ src/SimpleTask.js
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class SimpleTask extends Component {
   render() {
     return (
-      <div class="card">
+      <div class="card radios-border">
         <div class="card-section">
             {this.props.name} 
             {this.props.description}  
