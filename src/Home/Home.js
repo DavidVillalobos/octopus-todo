f1f96a4cafdca4330a33d90511ded6b1680f572f
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-07-23 02:11:57 -0600

    improve task views

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
+import { Grid, Cell } from 'react-foundation';
 import Task from "../DashBoard/Task";
 
 class Home extends Component {
-  task = { name: "example", duedate: "dd-mm-yyyy", description: "aja", state: 1 };
+  task = { name: "My task", duedate: "20/07/2021", state: 0, bgColor: "#1373aa", textColor: "white" }
   render() {
     return (
-      <div className="nav-page">
-        <Task content={this.task} />
+      <div className="home-task">
+        <Grid style={{ userSelect: "none" }}>
+          <Cell large={12}>
+            Hello
+          </Cell>
+          <Cell large={12}>
+            <Task content={this.task} />
+          </Cell>
+        </Grid>
       </div>
     );
   }
--- src/Tasks/ListTasks.js
+++ src/Tasks/ListTasks.js
@@ -1,13 +1,26 @@
 import React, { Component } from "react";
+import { Grid, Cell } from 'react-foundation';
 import Task from "../DashBoard/Task";
 
 class ListTasks extends Component {
-  task = { name: "example", duedate: "dd-mm-yyyy", description: "aja", state: 1 };
+  task = { name: "My task", duedate: "20/07/2021", state: 0, bgColor: "#1373aa", textColor: "white" }
   render() {
     return (
-      <div className="nav-page">
-        <Task content={this.task} />
-      </div>
+      <Grid style={{ userSelect: "none" }}>
+        <Cell small={3} large={3} className="list-task">
+        </Cell>
+        <Cell small={9} large={9} className="list-task">
+          <Task content={this.task} />
+          <Task content={this.task} />
+          <Task content={this.task} />
+          <Task content={this.task} />
+          <Task content={this.task} />
+          <Task content={this.task} />
+          <Task content={this.task} />
+          <Task content={this.task} />
+
+        </Cell>
+      </Grid>
     );
   }
 }
