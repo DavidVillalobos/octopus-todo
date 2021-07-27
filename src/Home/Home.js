c754bcbd592da74e9c1e47ec12398426c64dd0c5
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-07-26 18:14:24 -0600

    style: home page

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
+import { v4 as uuidv4 } from 'uuid'
 import { Grid, Cell } from 'react-foundation';
 import Task from "../DashBoard/Task";
 
 class Home extends Component {
-  task = { name: "My task", duedate: "20/07/2021", state: 0, bgColor: "#1373aa", textColor: "white" }
+
+  loadImportantItemsFromDisk() {
+    return [
+      { id: uuidv4(), name: "Math task", duedate: "20/07/2021", state: 0, bgColor: "#1373aa", textColor: "white" },
+      { id: uuidv4(), name: "Study Science", duedate: "21/06/2021", state: 0, bgColor: "#1373aa", textColor: "white" },
+      { id: uuidv4(), name: "Buy dinner", duedate: "22/08/2021", state: 2, bgColor: "#1373aa", textColor: "white" },
+      { id: uuidv4(), name: "Eat", duedate: "23/06/2021", state: 1, bgColor: "#1373aa", textColor: "white" },
+    ];
+  }
+
   render() {
     return (
       <div className="home-task">
         <Grid style={{ userSelect: "none" }}>
           <Cell large={12}>
-            Hello
+            WELCOME TO MAIN PAGE OF OCTOPUS TODO
           </Cell>
-          <Cell large={12}>
-            <Task content={this.task} />
+          <Cell offsetOnSmall={1} offsetOnLarge={1} large={10} small={10}>
+            {this.loadImportantItemsFromDisk().map(item => {
+              return (<Task content={item} />)
+            })
+            }
           </Cell>
         </Grid>
       </div>
