6b13c2e0d35daa11f7a097f1b17ea3cbba2207b9
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-07-26 19:53:43 -0600

    improve

--- src/Calendar/Day.js
+++ src/Calendar/Day.js
@@ -1,14 +1,25 @@
 import React, { Component } from "react";
+import { Grid, Cell } from "react-foundation";
+import TaskDay from "./TaskDay";
 
 class Day extends Component {
   style = (this.props.month === this.props.day.monthIndex ? "white" : "gray");
   render() {
     return (
       <div className="card day">
-        <div className={"card-section " + this.style}>
-          {this.props.day.day}
+        <div className={this.style}>
+          <Grid className="aja">
+            <Cell className="text-center">
+              <div className="number-day">
+                {this.props.day.day}
+              </div>
+            </Cell>
+            <Cell className="task-day-panel">
+              <TaskDay />
+            </Cell>
+          </Grid>
         </div>
-      </div>
+      </div >
     );
   }
 }
---
+++ src/Calendar/TaskDay.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import { Badge } from "react-foundation";
+
+class TaskDay extends Component {
+    render() {
+        return (
+            <Badge className="task-list-day">
+                <div className="name-task-day">
+                    MT
+                </div>
+            </Badge >
+        );
+    }
+}
+
+export default TaskDay;
+
