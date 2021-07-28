8319f01cf84b4b919e8c280d4bc70592b6acea93
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-07-28 02:16:25 -0600

    style: update style in task list

--- src/Tasks/ListTasks.js
+++ src/Tasks/ListTasks.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import { v4 as uuidv4 } from 'uuid'
-import { Grid, Cell, Menu, Button } from 'react-foundation';
+import { Grid, Cell, Menu, Button, Label } from 'react-foundation';
 import { PlusCircleIcon, CollectionIcon } from '@heroicons/react/solid'
 import Task from "../DashBoard/Task";
 
@@ -9,25 +9,24 @@ class ListTasks extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      currentTaskList: this.props.taskList[0],
       name: "",
-      description: "",
       duedate: ""
     };
     this.createTask = this.createTask.bind(this)
   }
 
-  currentTaskList = this.props.taskList[0];
 
-  handleClick(e) {
+  /*handleClick(e) {
     if (e.type === 'click') {
       console.log('Left click');
     } else if (e.type === 'contextmenu') {
       console.log('Right click');
     }
-  }
+  }*/
 
   createTask() {
-    console.log(`name: ${this.state.name}, description: ${this.state.description}, duedate: ${this.state.duedate}`);
+    console.log(`name: ${this.state.name}, duedate: ${this.state.duedate} in list ${this.state.currentTaskList.name}`);
   }
 
   render() {
@@ -35,12 +34,18 @@ class ListTasks extends Component {
       <Grid>
         <Cell small={3} large={3} className="lists-section">
           <Menu isVertical className="todo-lists">
-            <Button onClick={this.handleClick} onContextMenu={this.handleClick}>
+            <Button onClick={() => this.setState({ currentTaskList: this.props.taskList[0] })} onContextMenu={this.handleClick}>
               <CollectionIcon className="navbar-icon" />
-              Tasks
+              {this.props.taskList[0].name}
             </Button>
-            {this.props.taskList.map((list) => {
-              return (<Button key={uuidv4()} style={{ background: list.bgColor, color: list.color }}> {list.name} </Button>)
+            {this.props.taskList.slice(1).map((list) => {
+              return (
+                <Button
+                  key={uuidv4()}
+                  onClick={() => this.setState({ currentTaskList: this.props.taskList[list.listId] })}
+                  style={{ background: list.bgColor, color: list.textColor }}>
+                  {list.name}
+                </Button>)
             })}
             <Button>
               <PlusCircleIcon className="navbar-icon" />
@@ -54,22 +59,25 @@ class ListTasks extends Component {
               <Grid>
                 <Cell offsetOnSmall={1} offsetOnLarge={1} small={4} large={4}>
                   <input type="text" placeholder="name" onChange={(e) => this.setState({ name: e.target.value })} />
+                </Cell>
+                <Cell offsetOnSmall={1} offsetOnLarge={1} small={4} large={4}>
                   <input type="date" placeholder="DueDate" onChange={(e) => this.setState({ duedate: e.target.value })} />
                 </Cell>
-                <Cell offsetOnSmall={1} offsetOnLarge={1} small={5} large={5}>
-                  <textarea placeholder="Description" onChange={(e) => this.setState({ description: e.target.value })} />
-                </Cell>
-                <Cell offsetOnSmall={1} offsetOnLarge={1} small={10} large={10} className="text-center">
+                <Cell small={2} large={2} className="text-center">
                   <Button className="button-create" onClick={this.createTask}>
-                    <PlusCircleIcon className="navbar-icon" />
-                    Create task
+                    <PlusCircleIcon className="add-icon" />
                   </Button>
+                </Cell>
+                <Cell offsetOnSmall={1} offsetOnLarge={1} small={10} large={10}>
+                  <Label className="cant-task-column" style={{ userSelect: "none" }}>
+                    {this.state.currentTaskList.tasks.length + " in " + this.state.currentTaskList.name}
+                  </Label>
                 </Cell>
               </Grid>
             </Cell>
             <Cell small={12} large={12} className="panel-todo-list">
               <Grid>
-                {this.currentTaskList.tasks.map((task) => {
+                {this.state.currentTaskList.tasks.map((task) => {
                   return (
                     <Cell key={uuidv4()} offsetOnLarge={1} offsetOnSmall={1} small={10} large={10}>
                       <Task content={task} />
