d49a5c77f3fc9f1222ab382fb7f3cc4df66c9bb8
Luis David Villalobos González <villalobosgonzalezluisdavid@gmail.com>
2021-07-31 04:52:20 -0600

    Update Task.js

--- src/Task.js
+++ src/Task.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import { Grid, Cell } from 'react-foundation';
-import { ClockIcon, CheckIcon, ChartSquareBarIcon, ViewListIcon, MinusIcon } from '@heroicons/react/solid'
+import {
+  ClockIcon, CheckIcon, ChartSquareBarIcon,
+  ViewListIcon, BellIcon
+} from '@heroicons/react/solid'
 
 class Task extends Component {
   render() {
-    let icon = (this.props.content.completed) ? <CheckIcon className="navbar-icon" /> : <MinusIcon className="navbar-icon" />;
+    let icon = (this.props.content.completed) ? <CheckIcon className="navbar-icon" /> : <BellIcon className="navbar-icon" />;
     return (
       <div className="card card-task">
         <div className="card-section" style={{
@@ -12,19 +15,19 @@ class Task extends Component {
           color: this.props.content.textColor
         }}>
           <Grid>
-            <Cell small={8} large={8}>
+            <Cell small={7} large={7}>
               {icon}
               {this.props.content.name}
             </Cell>
-            <Cell small={4} large={4}>
+            <Cell small={5} large={5}>
               <ClockIcon className="navbar-icon" />
               {this.props.content.dueDate}
             </Cell>
-            <Cell small={8} large={8}>
+            <Cell small={7} large={7}>
               <ViewListIcon className="navbar-icon" />
               {this.props.content.nameTaskList}
             </Cell>
-            <Cell small={4} large={4}>
+            <Cell small={5} large={5}>
               <ChartSquareBarIcon className="navbar-icon" />
               {this.props.content.nameState}
             </Cell>
